0460c210afbb57353420763d9c5be22175dc2fdc
CybMeta <juanpadialmolina@gmail.com>
2014-08-08 12:04:32 +0200

    Update layout-picker-custom-control.php
    The layout picker was not working at all. It had a `<label>` wrapping all inputs. Only one option could be selected. `$this->link()` was missed in the inputs and current value was not checked.

--- layout/layout-picker-custom-control.php
+++ layout/layout-picker-custom-control.php
@@ -27,14 +27,23 @@ class Layout_Picker_Custom_Control extends WP_Customize_Control
                 $finalImageDirectory = get_stylesheet_directory_uri().$imageDirectoryInc;
             }
             ?>
-                <label>
-                  <span class="customize-layout-control"><?php echo esc_html( $this->label ); ?></span>
-                  <ul>
-                    <li><img src="<?php echo $finalImageDirectory; ?>1col.png" alt="Full Width" /><input type="radio" name="<?php echo $this->id; ?>" id="<?php echo $this->id; ?>[full_width]" value="1" /></li>
-                    <li><img src="<?php echo $finalImageDirectory; ?>2cl.png" alt="Left Sidebar" /><input type="radio" name="<?php echo $this->id; ?>" id="<?php echo $this->id; ?>[left_sidebar]" value="1" /></li>
-                    <li><img src="<?php echo $finalImageDirectory; ?>2cr.png" alt="Right Sidebar" /><input type="radio" name="<?php echo $this->id; ?>" id="<?php echo $this->id; ?>[right_sidebar]" value="1" /></li>
-                  </ul>
-                </label>
+	        <span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+	
+	       <label>
+	            <img src="<?php echo $finalImageDirectory; ?>1col.png" alt="Full Width" />
+		        <input type="radio" value="full_width" name="<?php echo esc_attr( $this->id ); ?>" <?php $this->link(); checked( $this->value(), "full_width"); ?> />
+		        <br/>
+	        </label>
+        	<label>
+	           	<img src="<?php echo $finalImageDirectory; ?>2cl.png" alt="Left Sidebar" />
+	        	<input type="radio" value="left_sidebar" name="<?php echo esc_attr( $this->id ); ?>" <?php $this->link(); checked( $this->value(), "left_sidebar"); ?> />
+	        	<br/>
+	        </label>
+        	<label>
+	        	<img src="<?php echo $finalImageDirectory; ?>2cr.png" alt="Right Sidebar" />
+	        	<input type="radio" value="right_sidebar" name="<?php echo esc_attr( $this->id ); ?>" <?php $this->link(); checked( $this->value(), "right_sidebar"); ?> />
+	        	<br/>
+        	</label>
             <?php
        }
 }
